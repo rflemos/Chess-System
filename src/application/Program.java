8d09d0117ad659a662279b284e9be59c7417bf18
RAFAEL <rafafaine@gmail.com>
2019-11-04 19:46:42 -0300

    Test Repository

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,7 @@ import boardgame.Position;
 public class Program {
 
 	public static void main(String[] args) {
-		Position pos = new Position(3,4);
+		Position pos = new Position(3,7);
 		
 		System.out.println(pos);
 	}
